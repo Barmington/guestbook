8de8dae00082de23ca3084e0d1de5d0d1ac69121
Barmington Chuku <barmingtonk@gmail.com>
2024-10-28 01:59:56 +0000

    completed guestbook

--- client/script.js
+++ client/script.js
@@ -1,10 +1,7 @@
 const formInfo = document.getElementById("form-Info");
 const guestInfo = document.querySelector(".guest-info");
-const todayDate = document.querySelector(".date");
 const makeComment = document.querySelector(".make-comment");
 const showCommentBox = document.querySelector(".comment");
-
-let viewercount = 0;
 
 async function handleSubmit(event) {
   event.preventDefault();
@@ -28,32 +25,19 @@ async function getGuest() {
   for (let i = 0; i < guests.length; i++) {
     const guestname = guests[i].guestname;
     const comment = guests[i].comment;
-
-    const newH3 = document.createElement("h3");
+    const newH3 = document.createElement("h4");
     const newP = document.createElement("p");
     const comContainer = document.createElement("div");
 
     newH3.textContent = `${guestname}`;
     newP.textContent = `${comment}`;
-    newP.style.width = "350px";
-    newP.style.height = "180px";
-
-    // newH3.textContent = `${guestname}`;
 
     guestInfo.appendChild(comContainer);
     comContainer.appendChild(newH3);
     comContainer.appendChild(newP);
-
-    const today = new Date("11, 26, 2024");
-    const month = today.getMonth();
-    const date = today.getDate();
-    const year = today.getFullYear();
-
-    todayDate.textContent = `${month}, ${date}, ${year}`;
+    comContainer.classList.add("com-container");
   }
-  function writeComment() {
-    showCommentBox.classList.remove("hide");
-  }
+
   makeComment.addEventListener("click", writeComment);
 }
 
